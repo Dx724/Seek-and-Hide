6581e14205e4a2342f0d9c33657d2605f8ce101a
Dx724 <dx72dx@gmail.com>
2021-03-05 01:22:28 -0500

    Add PIR

--- ESP32/SeekAndHide/SeekAndHide.ino
+++ ESP32/SeekAndHide/SeekAndHide.ino
@@ -7,12 +7,16 @@ void setup() {
   ultrasonic.setTemperature(21); // ~70 degrees Fahrenheit
   pinMode(18, INPUT_PULLUP);
   pinMode(19, INPUT_PULLUP);
+  pinMode(15, INPUT);
 }
 
 void loop() {
   int distance = ultrasonic.distanceInCentimeters();
-  int button1 = digitalRead(18);
-  int button2 = digitalRead(19);
-  Serial.printf("Distance: %d cm\tA: %d\tB:%d\n", distance, button1, button2);
+  int button1 = digitalRead(18); // Yellow button
+  int button2 = digitalRead(19); // Blue button
+
+  int pir = digitalRead(15); // Infrared sensor (jumper set to high for repeatable trigger mode), time set to minimum (3s) for smooth but responsive behavior
+  
+  Serial.printf("Distance: %d cm\tA: %d\tB:%d\tIR:%d\n", distance, button1, button2, pir);
   delay(50); // Run at high speed to ensure we can detect brief "string-crossing"
 }
